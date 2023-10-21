17758bd1c93da6579e8b26e8a5fbf29d7c23cb6f
JJerome-NM <jjerome010@gmail.com>
2023-10-21 18:00:40 +0300

    [PY-WEB-3]: Implemented all pages

--- lab3/Khalabarchuk_Project/app.py
+++ lab3/Khalabarchuk_Project/app.py
@@ -1,5 +1,40 @@
 from flask import Flask, render_template, request, url_for
 
+PROJECTS_LIST = list([
+    {"photo_url": "projectNMWS.png",
+     "name": "NM-WebSockets",
+     "project_url": "https://github.com/JJerome-NM/NM-WebSockets"
+     },
+    {"photo_url": "projectCodenames.png",
+     "name": "CodeNames",
+     "project_url": "https://github.com/JJerome-NM/CodeNames"
+     },
+    {"photo_url": "projectWSHelper.png",
+     "name": "WebSocketHelper",
+     "project_url": "https://github.com/JJerome-NM/WebSocketHelper"
+     }]
+)
+
+CONTACTS = [{
+        "type": "EMAIL",
+        "contact_name": "Gmail",
+        "name": "Mykhailo Khalabarchuk",
+        "link": "mykhailo.khalabarchuk.21@pnu.edu.ua",
+        "title": "JJerome-NM"
+    }, {
+        "type": "DEFAULT",
+        "contact_name": "Linkedin",
+        "name": "Mykhailo Khalabarchuk",
+        "link": "https://www.linkedin.com/in/mykhailo-khalabarchuk-26376528b/",
+        "title": "Mykhailo Khalabarchuk"
+    }, {
+        "type": "DEFAULT",
+        "contact_name": "Github",
+        "name": "Михайло Халабарчук",
+        "link": "https://github.com/JJerome-NM",
+        "title": "Михайло Халабарчук"
+    }]
+
 app = Flask(__name__)
 
 
@@ -11,12 +46,12 @@ def portfolio_main():
 
 @app.route('/projects')
 def portfolio_projects():
-    return render_template("portfolio-projects.html")
+    return render_template("portfolio-projects.html", projects_list=PROJECTS_LIST)
 
 
 @app.route('/contacts')
 def portfolio_contacts():
-    return render_template("portfolio-contacts.html")
+    return render_template("portfolio-contacts.html", contacts=CONTACTS)
 
 
 if __name__ == '__main__':
