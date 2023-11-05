ea043e27f90d0a0d70fe5d155d7bbba22f388d9e
JJerome-NM <jjerome010@gmail.com>
2023-11-05 20:14:23 +0200

    [PY-WEB-15]: Implemented task 7

--- lab4/Khalabarchuk_Project/entitys/User.py
+++ lab4/Khalabarchuk_Project/entitys/User.py
@@ -1,20 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SubmitField, EmailField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp
 
 from app import db, bcrypt
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired("This field is required")])
+    username = StringField('Username', validators=[
+        DataRequired("This field is required"),
+        Length(4, 30, "The length must be greater than 4 and less than 30"),
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Nickname can include only letters, numbers, underscore and a dot')
+
+    ])
     email = EmailField('Email(Login)', validators=[DataRequired("This field is required")])
     password = PasswordField('Password', validators=[
         DataRequired("This field is required"),
-        Length(min=4, max=30, message="The length must be greater than 3 and less than 30")
+        Length(min=4, max=30, message="The length must be greater than 4 and less than 30")
     ])
     confirm_password = PasswordField('Confirm password', validators=[
         DataRequired("This field is required"),
-        Length(min=4, max=30, message="The length must be greater than 3 and less than 30")
+        Length(min=4, max=30, message="The length must be greater than 4 and less than 30")
     ])
     submit = SubmitField("Sign-up")
 
@@ -24,6 +29,7 @@ class AuthUser(db.Model):
     email = db.Column(db.String(35), unique=True, nullable=False)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password_hash = db.Column(db.String(60), unique=False, nullable=False)
+    avatar_image = db.Column(db.String(30), nullable=True, default="default.png")
 
     @property
     def password(self):
@@ -35,6 +41,16 @@ class AuthUser(db.Model):
 
     def verify_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def validate_username(username):
+        if AuthUser.query.filter_by(username=username).first():
+            raise ValidationError("Username is busy")
+
+    @staticmethod
+    def validate_email(email):
+        if AuthUser.query.filter_by(email=email).first():
+            raise ValidationError("Email is busy")
 
 
 class User:
