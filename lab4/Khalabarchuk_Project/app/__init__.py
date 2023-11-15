32d618c70b90390245c57ee42bda3a0210c4bd8c
JJerome-NM <jjerome010@gmail.com>
2023-11-15 19:24:11 +0200

    Small rework config

--- lab4/Khalabarchuk_Project/app/__init__.py
+++ lab4/Khalabarchuk_Project/app/__init__.py
@@ -4,24 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-import config
+from config import Config
 
 def enumerate_filter(iterable):
     return enumerate(iterable)
 
 
+config: Config = Config.get_config()
+
 app = Flask(__name__)
-app.secret_key = b"secret"
-
-app.config.from_object('config')
-
+app.config.from_object(config)
 app.jinja_env.filters['enumerate'] = enumerate_filter
 
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
 login_manager.login_view = config.LOGIN_MANAGER_LOGIN_VIEW
 login_manager.login_message_category = config.LOGIN_MANAGER_LOGIN_MESSAGE_CATEGORY
 login_manager.login_message = config.LOGIN_MANAGER_LOGIN_MESSAGE
@@ -59,4 +58,4 @@ def create_app(config_class):
     return app_with_bp
 
 
-app = create_app("config")
+app = create_app(config)
