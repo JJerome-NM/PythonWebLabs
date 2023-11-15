b4c3f5153fbb54fb13f5ffe66512c1771d6e14a2
JJerome-NM <jjerome010@gmail.com>
2023-11-15 18:44:56 +0200

    Removed old views, controllers, forms and entitys

--- lab4/Khalabarchuk_Project/app/forms/ToDo.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class ToDoForm(FlaskForm):
-    title = StringField("", validators=[DataRequired(message="This field is required.")])
-    submit = SubmitField("Save")
