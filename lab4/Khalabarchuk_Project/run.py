4d01ee319e943ad1b57ad84f26cdc62b17444d9d
JJerome-NM <jjerome010@gmail.com>
2023-12-31 17:48:59 +0200

    [RAILWAY] Added host env variable

--- lab4/Khalabarchuk_Project/run.py
+++ lab4/Khalabarchuk_Project/run.py
@@ -4,4 +4,4 @@ from app import app
 
 
 if __name__ == '__main__':
-    app.run(port=environ.get("PORT") or "5000")
+    app.run(port=environ.get("PORT") or "5000", host=environ.get("HOST") or "localhost")
